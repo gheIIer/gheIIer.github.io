d6a509781fea902438c3c1105e35c42dc8627159
gheller <87474222+gheller3310@users.noreply.github.com>
2022-09-05 16:48:49 +0200

    First Post Updated - Preview Unavailable

---
+++ js/objects.js
@@ -0,0 +1,18 @@
+/*==================== VARIABLES ====================*/
+// GENERAL
+var website = "Website •"
+var project = "Project •"
+var notes = "Notes •"
+
+// RELEASES
+var v1 = "v1.3.0"
+
+// TEXT
+var title1 = "Behold... The Blog!"
+var subtitle1 = "The blog section is currently under construction!"
+
+// DATES
+var date1 = "5 Sep, 2022 •"
+
+// READING TIME
+var min1 = " 1 min read"
