ccaa49b78b89ec8e1cfe270e1673cb535f7fbd22
gheller <87474222+gheller3310@users.noreply.github.com>
2022-09-05 18:18:30 +0200

    Small Changes to UI

--- js/objects.js
+++ js/objects.js
@@ -6,13 +6,19 @@ var notes = "Notes •"
 
 // RELEASES
 var v1 = "v1.3.0"
+var v2 = "v1.3.1"
 
 // TEXT
 var title1 = "Behold... The Blog!"
 var subtitle1 = "The blog section is currently under construction!"
 
+var title2 = "The journey begins"
+var subtitle2 = "The first 2 articles have just been uploaded in the blog section"
+
 // DATES
 var date1 = "5 Sep, 2022 •"
+var date2 = "6 Sep, 2022 •"
 
 // READING TIME
-var min1 = " 1 min read"
+var min1 = "1 min read"
+var min2 = "2 min read"
