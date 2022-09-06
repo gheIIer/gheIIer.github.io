050efd92313e3292407951b23be9ede1f3188364
gheller <87474222+gheller3310@users.noreply.github.com>
2022-09-06 13:26:03 +0200

    Ethereum Notes Added

--- js/objects.js
+++ js/objects.js
@@ -5,6 +5,7 @@ var project = "Project •"
 var notes = "Notes •"
 
 // RELEASES
+var first = "v1.0.0"
 var v1 = "v1.3.0"
 var v2 = "v1.3.1"
 
@@ -15,10 +16,14 @@ var subtitle1 = "The blog section is currently under construction!"
 var title2 = "The journey begins"
 var subtitle2 = "The first 2 articles have just been uploaded in the blog section"
 
+var title3 = "Personal Ethereum Notebook"
+var subtitle3 = "All my notes reguarding the Ethereum Blockchain and Solidity Projects"
+
 // DATES
 var date1 = "5 Sep, 2022 •"
 var date2 = "6 Sep, 2022 •"
 
 // READING TIME
+var undefinedTime = "///"
 var min1 = "1 min read"
 var min2 = "2 min read"
